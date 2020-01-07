6fb0716647761868f2fadb50768f87fffb57af15
Simon Bull <simon.bull.84@googlemail.com>
2020-01-07 11:34:31 +0000

    Cache establishment UKPRN

--- src/Dfe.Spi.GiasAdapter.Domain/GiasApi/Establishment.cs
+++ src/Dfe.Spi.GiasAdapter.Domain/GiasApi/Establishment.cs
@@ -4,5 +4,6 @@ namespace Dfe.Spi.GiasAdapter.Domain.GiasApi
     {
         public long Urn { get; set; }
         public string Name { get; set; }
+        public long? Ukprn { get; set; }
     }
 }
